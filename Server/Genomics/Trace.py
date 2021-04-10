8262458171f0572219a15bb8c43cbc42a8d85737
eric12s <ericsabag@gmail.com>
2021-04-11 02:06:20 +0300

    Working on getting TRACE data

--- Server/Genomics/Trace.py
+++ Server/Genomics/Trace.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine, MetaData, Table
-from sqlalchemy.orm import sessionmaker
+import requests
+import json
 
 
 # def generate_csv(request):
@@ -33,17 +33,43 @@ def generate_table_from_vcf(vcf, tissue):
     # print('tissue: %s, genes: %s' % (tissue, str(vcf)))
     genes = set([])
 
-    # s_vcf = vcf.split('\n')
-    # vars = []
-    # for line in s_vcf:
-    #     if len(line) > 0 and not line[0] == '#' and not ('CHR' in line):
-    #         vars.append(line)
-    # variants = {'variants': vars}
-    send_query_to_Trace("some query")
+    s_vcf = vcf.split('\n')
+    vars = []
+    for line in s_vcf:
+        if len(line) > 0 and not line[0] == '#' and not ('CHR' in line):
+            vars.append(line)
+    variants = {'variants': vars}
+    headers = {"Content-Type": "application/json", "Accept": "application/json"}
+    res = requests.post("https://rest.ensembl.org/vep/homo_sapiens/region", headers=headers, data=json.dumps(variants))
 
-def send_query_to_Trace(Trace_query):
+    if not res.ok:
+        print('Error getting genes names for TRACE')
+    else:
+        print('res', res.json())
+        res_data = res.json()
+        for item in res_data:
+            if "transcript_consequences" in item:
+                for var in item["transcript_consequences"]:
+                    if "gene_id" in var:
+                        genes.add(var["gene_id"])
+                        
+    print(genes)
+    send_query_to_Trace(list(genes))
+
+def send_query_to_Trace(genes):
     print("Sending Query to Trace")
     #TODO: Send query to Trace
+    from models import Df_Complete_Dataset
+
+    #TODO: May need to use handle_genes_names function from TRACE
+    q = Df_Complete_Dataset.query.filter(Df_Complete_Dataset.ID.in_(genes)).all()
+    from sqlalchemy import inspect
+    inst = inspect(Df_Complete_Dataset)
+    attr_names = [c_attr.key for c_attr in inst.mapper.column_attrs]
+    print(attr_names)
+    for row in q:
+        for attr in attr_names:
+            print(row.ID + " " + attr + " " + str(getattr(row, attr)))
 
     # engine = create_engine('mysql+pymysql://root:BiVc18@genomics.bgu.ac.il:3306/netbio1', echo=True)
     # metadata = MetaData(engine)
--- Server/config/prod.py
+++ Server/config/prod.py
@@ -6,11 +6,12 @@ DEBUG = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 # Main database bind URI.
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:BiVc18@genomics.bgu.ac.il:33306/netbio1'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:BiVc18@genomics.bgu.ac.il:33306/netbio2'
 
 # Auxiliary bindings.
 SQLALCHEMY_BINDS = {
     # 'InteractionsData': 'mysql+pymysql://ms_trace_dbu:bgu2010@netbio.bgu.ac.il:33306/InteractionsData',
     # 'TissueExpressionData': 'mysql+pymysql://ms_trace_dbu:bgu2010@netbio.bgu.ac.il:33306/TissueExpressionData',
-    # 'Interactions': 'mysql+pymysql://ms_trace_dbu:bgu2010@netbio.bgu.ac.il:33306/Interactions'
+    # 'Interactions': 'mysql+pymysql://ms_trace_dbu:bgu2010@netbio.bgu.ac.il:33306/Interactions',
+    'netbio2': 'mysql+pymysql://root:BiVc18@genomics.bgu.ac.il:3306/netbio2'
 }
