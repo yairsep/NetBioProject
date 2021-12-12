063b991f909f258cdb3067485a3134ed7144b7e7
gilgru23 <48647298+gilgru23@users.noreply.github.com>
2021-12-12 10:16:09 +0200

    Update dev.py

--- Server/config/dev.py
+++ Server/config/dev.py
@@ -5,13 +5,13 @@ DEBUG = True
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 # Main database bind URI.
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://ms_trace_dbu:bgu2010@netbio.bgu.ac.il:33306/TRACE'
-# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:BiVc18@genomics.bgu.ac.il:3306/netbio2'
+# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://ms_trace_dbu:bgu2010@netbio.bgu.ac.il:33306/TRACE'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:BiVc18@genomics.bgu.ac.il:3306/netbio2'
 # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://username:password@localhost/DifferentialInteractomes'
 
 # Auxiliary bindings.
 SQLALCHEMY_BINDS = {
     'TRACE': 'mysql+pymysql://ms_trace_dbu:bgu2010@netbio.bgu.ac.il:33306/TRACE',
     'Interactions': 'mysql+pymysql://ms_trace_dbu:bgu2010@netbio.bgu.ac.il:33306/Interactions',
-    # 'netbio2': 'mysql+pymysql://root:BiVc18@genomics.bgu.ac.il:3306/netbio2'
+    'netbio2': 'mysql+pymysql://root:BiVc18@genomics.bgu.ac.il:3306/netbio2'
 }
